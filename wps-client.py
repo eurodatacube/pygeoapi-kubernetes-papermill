f3a500d0b79086dcaee6880888b3da3eac8b7ca5
Bernhard Mallinger <bernhard.mallinger@eox.at>
2021-01-07 13:04:48 +0100

    Extend wps client for more use cases

--- wps-client.py
+++ wps-client.py
@@ -1,23 +1,28 @@
 import click
 import re
-import yaml
 import requests
+import pprint
+import json
 
 
 @click.command()
-@click.argument('notebook', type=click.Path(exists=True))
-@click.argument('output_path', type=click.Path(exists=False))
-@click.argument('parameters_path', type=click.Path(exists=True),)
-@click.argument('wps_endpoint', type=click.STRING)
-@click.option('--cpu', default=None, help='number of CPUs format: request/limit')
-@click.option('--mem', default=None, help='memory in GB format: request/limit')
-def main(notebook, output_path, parameters_path, wps_endpoint, cpu, mem):
-    def parse_limit_reqest(param):
+@click.argument("wps_endpoint", type=click.STRING)
+@click.argument("notebook", type=click.Path())
+@click.option("--parameters", default=None, type=json.loads)
+@click.option("--output_path", default=None, type=click.Path())
+@click.option("--cpu", default=None, help="number of CPUs format: request/limit")
+@click.option("--mem", default=None, help="memory in GB format: request/limit")
+def main(notebook, output_path, parameters, wps_endpoint, cpu, mem):
+    parameters = parameters or {}
 
-        re_float = "(\d+(?:\.\d*)?)"
+    def parse_limit_reqest(param):
+        re_float = r"(\d+(?:\.\d*)?)"
         if not param:
             return "", ""
-        split = re.findall(r"^{re_float}/{re_float}$".format(re_float=re_float), param)[0]
+        split = re.findall(
+            r"^{re_float}/{re_float}$".format(re_float=re_float),
+            param,
+        )[0]
         return float(split[0]), float(split[1])
 
     def parse_mem(value):
@@ -32,40 +37,25 @@ def main(notebook, output_path, parameters_path, wps_endpoint, cpu, mem):
         return str(limit), str(request)
 
     def parameter(key, value):
-
-        return dict(
-            id=key,
-            value=value
-        )
+        return dict(id=key, value=value)
 
     cpu_request, cpu_limit = parse_cpu(cpu)
     mem_request, mem_limit = parse_mem(mem)
 
-    with open(parameters_path) as f:
-        parameters = yaml.load(f, Loader=yaml.FullLoader)
-
+    # trick to "record" execution parameters
     parameters["_execution"] = dict(
         notebook=notebook,
         output_path=output_path,
-        parameters_path=parameters_path,
         cpu_request=cpu_request,
         cpu_limit=cpu_limit,
         mem_request=mem_request,
-        mem_limit=mem_limit
+        mem_limit=mem_limit,
     )
-
-    headers = {
-        'Content-type': 'application/json',
-    }
-
-    url = 'https://{wps_endpoint}/processes/execute-notebook/jobs?async-execute=True'.format(wps_endpoint=wps_endpoint)
 
     inputs = [
         parameter("notebook", notebook),
-        parameter("output_path", output_path),
         parameter("parameters_json", parameters),
-
-    ]
+    ] + ([parameter("output_path", output_path)] if output_path else [])
 
     if cpu:
         inputs.append(parameter("cpu_request", cpu_request))
@@ -75,8 +65,17 @@ def main(notebook, output_path, parameters_path, wps_endpoint, cpu, mem):
         inputs.append(parameter("mem_request", mem_request))
         inputs.append(parameter("mem_limit", mem_limit))
 
-    response = requests.post(url, headers=headers, data=str({"inputs": inputs}).replace("\'", "\""))
+    print("Sending request:")
+    pprint.pprint(inputs)
+    response = requests.post(
+        f"https://{wps_endpoint}/processes/execute-notebook/jobs",
+        headers={
+            "Content-type": "application/json",
+        },
+        json={"inputs": inputs},
+    )
     print(response)
+    print(response.content.decode())
 
 
 if __name__ == "__main__":
