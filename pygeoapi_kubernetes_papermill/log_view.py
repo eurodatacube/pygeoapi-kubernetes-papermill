b8884457ce97d866aa70dc50311617f826df23b2
Bernhard Mallinger <bernhard.mallinger@eox.at>
2024-12-03 17:41:33 +0100

    Another fix for the loki logs query

--- pygeoapi_kubernetes_papermill/log_view.py
+++ pygeoapi_kubernetes_papermill/log_view.py
@@ -32,6 +32,7 @@ import logging
 import itertools
 import time
 
+from flask import Response
 import requests
 
 # NOTE: this assumes flask_app, which is the default.
@@ -43,9 +44,10 @@ LOGGER = logging.getLogger(__name__)
 
 @APP.get("/jobs/<job_id>/logs")
 def get_job_logs(job_id):
-    LOGGER.debug(f"Retrieving job logs for {job_id}")
+    LOGGER.info(f"Retrieving job logs for {job_id}")
 
     from pygeoapi.flask_app import api_
+    from pygeoapi_kubernetes_papermill.common import k8s_job_name
 
     log_query_endpoint = getattr(api_.manager, "log_query_endpoint", None)
     if not log_query_endpoint:
@@ -63,21 +65,25 @@ def get_job_logs(job_id):
         # 21 days in ns (default limit in loki is 30 days)
         query_time_range = 21 * 24 * 60 * 60 * 1_000_000_000
 
+        job_name = k8s_job_name(job_id)
+
+        request_params = {
+            "query": f'{{job="{namespace}/{job_name}"}}',
+            "start": time.time_ns() - query_time_range,
+            "end": time.time_ns(),
+        }
         response = requests.get(
             log_query_endpoint,
-            params={
-                "query": f'{{job="{namespace}/{job_id}"}}',
-                "start": time.time_ns() - query_time_range,
-                "end": time.time_ns(),
-            },
+            params=request_params,
         )
         response.raise_for_status()
         streams = response.json()["data"]["result"]
 
-        # here let's just mix stdout/stderr
-        return "\n".join(
+        log_output = "\n".join(
             entry[1]
             for entry in sorted(
+                # here let's just mix stdout/stderr
                 itertools.chain.from_iterable(result["values"] for result in streams)
             )
         )
+        return Response(log_output, mimetype="text/plain")
